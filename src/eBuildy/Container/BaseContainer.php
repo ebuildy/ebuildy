02acc07ad718f5f5d5b3719c4c0c0584096ede46
eBuildy <ebuildy@gmail.com>
2015-01-27 10:05:54 +0100

    Add newInstance method
    This allow dependencies injection at runtime.

--- src/eBuildy/Container/BaseContainer.php
+++ src/eBuildy/Container/BaseContainer.php
@@ -10,4 +10,47 @@ class BaseContainer
   
 	return $this->$method(); 
     }
+    
+    /**
+     * Instance $class and resolve dependencies-injection dynamically.
+     *
+     * @param string $class
+     * @return mixed
+     */
+    public function newInstance($class)
+    {
+        $reflectedClass = new \ReflectionClass($class);
+
+        $properties = $reflectedClass->getProperties(\ReflectionMethod::IS_PUBLIC);
+
+        $instance = new $class();
+
+        foreach ($properties as $property)
+        {
+            $currentProperty = $property->getName();
+
+            foreach ($this->parseAnnotations($property->getDocComment()) as $annotation)
+            {
+                $annotationMethod = substr($annotation, 0, strpos($annotation, '('));
+
+                if ($annotationMethod === 'Inject')
+                {
+                    $service = substr($annotation, 2 + strpos($annotation, '("'), strpos($annotation, '")') - strpos($annotation, '("') - 2);
+
+                    $instance->$currentProperty = $this->get($service);
+                }
+            }
+        }
+
+        return $instance;
+    }
+
+    protected function parseAnnotations($annotations)
+    {
+        $buffer = array();
+
+        preg_match_all('#@(.*?)\n#s', $annotations, $buffer);
+
+        return $buffer[1];
+    }
 }
